b280361a5e3f2c81c517c70a9edf4442f5daea0d
Oliver Zander <oliver.zander@gmail.com>
2014-05-23 09:53:15 +0200

    added visual studio prject

---
+++ as-bibliothek-sql/as-bibliothek-sql/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace as_bibliothek_sql
+{
+  partial class login
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.txtBoxLogin = new System.Windows.Forms.TextBox();
+      this.txtBoxPasswort = new System.Windows.Forms.TextBox();
+      this.lblLogIn = new System.Windows.Forms.Label();
+      this.lblPasswort = new System.Windows.Forms.Label();
+      this.btnLogin = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // txtBoxLogin
+      // 
+      this.txtBoxLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.txtBoxLogin.Location = new System.Drawing.Point(128, 24);
+      this.txtBoxLogin.Name = "txtBoxLogin";
+      this.txtBoxLogin.Size = new System.Drawing.Size(137, 20);
+      this.txtBoxLogin.TabIndex = 0;
+      // 
+      // txtBoxPasswort
+      // 
+      this.txtBoxPasswort.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.txtBoxPasswort.Location = new System.Drawing.Point(128, 53);
+      this.txtBoxPasswort.Name = "txtBoxPasswort";
+      this.txtBoxPasswort.Size = new System.Drawing.Size(137, 20);
+      this.txtBoxPasswort.TabIndex = 1;
+      // 
+      // lblLogIn
+      // 
+      this.lblLogIn.AutoSize = true;
+      this.lblLogIn.BackColor = System.Drawing.Color.Transparent;
+      this.lblLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblLogIn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+      this.lblLogIn.Location = new System.Drawing.Point(30, 27);
+      this.lblLogIn.Name = "lblLogIn";
+      this.lblLogIn.Size = new System.Drawing.Size(92, 17);
+      this.lblLogIn.TabIndex = 2;
+      this.lblLogIn.Text = "Login Name: ";
+      // 
+      // lblPasswort
+      // 
+      this.lblPasswort.AutoSize = true;
+      this.lblPasswort.BackColor = System.Drawing.Color.Transparent;
+      this.lblPasswort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblPasswort.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+      this.lblPasswort.Location = new System.Drawing.Point(30, 53);
+      this.lblPasswort.Name = "lblPasswort";
+      this.lblPasswort.Size = new System.Drawing.Size(73, 17);
+      this.lblPasswort.TabIndex = 3;
+      this.lblPasswort.Text = "Passwort: ";
+      // 
+      // btnLogin
+      // 
+      this.btnLogin.BackColor = System.Drawing.Color.Transparent;
+      this.btnLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+      this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.btnLogin.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+      this.btnLogin.Location = new System.Drawing.Point(165, 95);
+      this.btnLogin.Name = "btnLogin";
+      this.btnLogin.Size = new System.Drawing.Size(100, 29);
+      this.btnLogin.TabIndex = 4;
+      this.btnLogin.Text = "anmelden";
+      this.btnLogin.UseVisualStyleBackColor = false;
+      // 
+      // login
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+      this.BackgroundImage = global::as_bibliothek_sql.Properties.Resources._171930__grass_grass_plants_herbs_green_nature_light_glare_bokeh_close_up_p;
+      this.ClientSize = new System.Drawing.Size(297, 151);
+      this.Controls.Add(this.btnLogin);
+      this.Controls.Add(this.lblPasswort);
+      this.Controls.Add(this.lblLogIn);
+      this.Controls.Add(this.txtBoxPasswort);
+      this.Controls.Add(this.txtBoxLogin);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.Name = "login";
+      this.Text = "Login";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox txtBoxLogin;
+    private System.Windows.Forms.TextBox txtBoxPasswort;
+    private System.Windows.Forms.Label lblLogIn;
+    private System.Windows.Forms.Label lblPasswort;
+    private System.Windows.Forms.Button btnLogin;
+  }
+}
+
---
+++ as-bibliothek-sql/as-bibliothek-sql/frm-main.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace as_bibliothek_sql
+{
+  public partial class frmMain : Form
+  {
+    private TabControl tabControl1;
+    private TabPage tbpgeBooks;
+    private TabPage tbPgeClients;
+    private TabPage tbPgeEmployes;
+    private TextBox txtBoxSearchBook;
+    private Label lblSearchBook;
+    private DataGridView dataGridView1;
+    private TabPage tbpPgeLibrary;
+  
+    public frmMain()
+    {
+      InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+      this.tabControl1 = new System.Windows.Forms.TabControl();
+      this.tbpgeBooks = new System.Windows.Forms.TabPage();
+      this.tbPgeClients = new System.Windows.Forms.TabPage();
+      this.tbPgeEmployes = new System.Windows.Forms.TabPage();
+      this.tbpPgeLibrary = new System.Windows.Forms.TabPage();
+      this.lblSearchBook = new System.Windows.Forms.Label();
+      this.txtBoxSearchBook = new System.Windows.Forms.TextBox();
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      this.tabControl1.SuspendLayout();
+      this.tbpgeBooks.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // tabControl1
+      // 
+      this.tabControl1.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
+      this.tabControl1.Controls.Add(this.tbpgeBooks);
+      this.tabControl1.Controls.Add(this.tbPgeClients);
+      this.tabControl1.Controls.Add(this.tbPgeEmployes);
+      this.tabControl1.Controls.Add(this.tbpPgeLibrary);
+      this.tabControl1.Location = new System.Drawing.Point(12, 12);
+      this.tabControl1.Multiline = true;
+      this.tabControl1.Name = "tabControl1";
+      this.tabControl1.SelectedIndex = 0;
+      this.tabControl1.Size = new System.Drawing.Size(648, 434);
+      this.tabControl1.TabIndex = 2;
+      // 
+      // tbpgeBooks
+      // 
+      this.tbpgeBooks.BackColor = System.Drawing.Color.Transparent;
+      this.tbpgeBooks.Controls.Add(this.dataGridView1);
+      this.tbpgeBooks.Controls.Add(this.txtBoxSearchBook);
+      this.tbpgeBooks.Controls.Add(this.lblSearchBook);
+      this.tbpgeBooks.ForeColor = System.Drawing.Color.Black;
+      this.tbpgeBooks.Location = new System.Drawing.Point(4, 25);
+      this.tbpgeBooks.Name = "tbpgeBooks";
+      this.tbpgeBooks.Padding = new System.Windows.Forms.Padding(3);
+      this.tbpgeBooks.Size = new System.Drawing.Size(640, 405);
+      this.tbpgeBooks.TabIndex = 0;
+      this.tbpgeBooks.Text = "Bücher";
+      // 
+      // tbPgeClients
+      // 
+      this.tbPgeClients.BackColor = System.Drawing.Color.Transparent;
+      this.tbPgeClients.ForeColor = System.Drawing.Color.MediumSpringGreen;
+      this.tbPgeClients.Location = new System.Drawing.Point(4, 25);
+      this.tbPgeClients.Name = "tbPgeClients";
+      this.tbPgeClients.Padding = new System.Windows.Forms.Padding(3);
+      this.tbPgeClients.Size = new System.Drawing.Size(457, 209);
+      this.tbPgeClients.TabIndex = 1;
+      this.tbPgeClients.Text = "Kunden";
+      // 
+      // tbPgeEmployes
+      // 
+      this.tbPgeEmployes.BackColor = System.Drawing.Color.Transparent;
+      this.tbPgeEmployes.ForeColor = System.Drawing.Color.MediumSpringGreen;
+      this.tbPgeEmployes.Location = new System.Drawing.Point(4, 25);
+      this.tbPgeEmployes.Name = "tbPgeEmployes";
+      this.tbPgeEmployes.Size = new System.Drawing.Size(457, 209);
+      this.tbPgeEmployes.TabIndex = 2;
+      this.tbPgeEmployes.Text = "Mitarbeiter";
+      // 
+      // tbpPgeLibrary
+      // 
+      this.tbpPgeLibrary.BackColor = System.Drawing.Color.Transparent;
+      this.tbpPgeLibrary.ForeColor = System.Drawing.Color.MediumSpringGreen;
+      this.tbpPgeLibrary.Location = new System.Drawing.Point(4, 25);
+      this.tbpPgeLibrary.Name = "tbpPgeLibrary";
+      this.tbpPgeLibrary.Size = new System.Drawing.Size(457, 209);
+      this.tbpPgeLibrary.TabIndex = 3;
+      this.tbpPgeLibrary.Text = "Bibliothek";
+      // 
+      // lblSearchBook
+      // 
+      this.lblSearchBook.AutoSize = true;
+      this.lblSearchBook.ForeColor = System.Drawing.Color.Black;
+      this.lblSearchBook.Location = new System.Drawing.Point(6, 8);
+      this.lblSearchBook.Name = "lblSearchBook";
+      this.lblSearchBook.Size = new System.Drawing.Size(41, 13);
+      this.lblSearchBook.TabIndex = 0;
+      this.lblSearchBook.Text = "Suche:";
+      this.lblSearchBook.Click += new System.EventHandler(this.lblSearchBook_Click);
+      // 
+      // txtBoxSearchBook
+      // 
+      this.txtBoxSearchBook.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.txtBoxSearchBook.Location = new System.Drawing.Point(53, 6);
+      this.txtBoxSearchBook.Name = "txtBoxSearchBook";
+      this.txtBoxSearchBook.Size = new System.Drawing.Size(581, 20);
+      this.txtBoxSearchBook.TabIndex = 1;
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.Location = new System.Drawing.Point(6, 49);
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.Size = new System.Drawing.Size(628, 350);
+      this.dataGridView1.TabIndex = 2;
+      // 
+      // frmMain
+      // 
+      this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+      this.ClientSize = new System.Drawing.Size(672, 458);
+      this.Controls.Add(this.tabControl1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+      this.Name = "frmMain";
+      this.tabControl1.ResumeLayout(false);
+      this.tbpgeBooks.ResumeLayout(false);
+      this.tbpgeBooks.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    private void lblSearchBook_Click(object sender, EventArgs e)
+    {
+
+    }
+
+
+  }
+}
