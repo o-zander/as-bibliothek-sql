0dcf9ecc96dde47c70edb02f5c6d20ea228034fc
Oliver Zander <oliver.zander@gmail.com>
2014-06-04 21:16:00 +0200

    added right management procedures

--- sql-queries/create-procedures.sql
+++ sql-queries/create-procedures.sql
@@ -7,19 +7,44 @@ CREATE PROCEDURE create_person
     BEGIN
       INSERT INTO T_Address (street, house_number, postal, city, country)
       VALUES (@street, @house_number, @postal, @city, @country);
-      
+    
       INSERT INTO T_IdentityCards (issue_date, card_status)
       VALUES (GETDATE(), 'Erstellt');
-      
+    
       INSERT INTO T_Accounts (balance)
       VALUES ('0.0');
-      
+    
       INSERT INTO T_Persons (f_address_id, f_identity_number, f_account_id, first_name, last_name, day_of_birth, username)
       VALUES (
         IDENT_CURRENT('T_Address'), IDENT_CURRENT('T_IdentityCards'), IDENT_CURRENT('T_Accounts'),
         @first_name, @last_name, @day_of_birth, IDENT_CURRENT('T_IdentityCards') 
       );
     END
+GO
+
+CREATE PROCEDURE grant_right
+  @person int, @right int
+
+  AS
+    INSERT INTO T_PersonHasRights (p_f_person_id, p_f_right_id)
+    VALUES (@person, @right);
+GO
+
+CREATE PROCEDURE revoke_right
+  @person int, @right int
+
+  AS
+    DELETE FROM T_PersonHasRights
+    WHERE p_f_person_id = @person AND p_f_right_id = @right;
+GO
+
+CREATE PROCEDURE has_right
+  @person int, @right int
+
+  AS
+    SELECT 1
+    FROM T_PersonHasRights
+    WHERE p_f_person_id = @person AND p_f_right_id = @right;
 GO
 
 CREATE PROCEDURE create_book
@@ -50,33 +75,33 @@ CREATE PROCEDURE create_exemplars
   @book int, @count int, @status varchar(255) = 'Neu'
 
   AS
-	BEGIN
-		IF @count > 0
-		BEGIN
-			DECLARE @field varchar(2), @author varchar(1), @signature varchar(255), @timestamp int;
+    BEGIN
+      IF @count > 0
+        BEGIN
+          DECLARE @field varchar(2), @author varchar(1), @signature varchar(255), @timestamp int;
 
-			SET @field = (
-				SELECT UPPER(field)
-				FROM T_Books
-				WHERE p_book_id = @book
-			);
+          SET @field = (
+            SELECT UPPER(field)
+            FROM T_Books
+            WHERE p_book_id = @book
+          );
 
-			SET @author = (
-				SELECT TOP 1 UPPER(last_name)
-				FROM T_Authors as A
-					JOIN T_AuthorsBooks as AB ON A.p_author_id = AB.p_f_author_id
-				WHERE AB.p_f_book_id = @book
-			);
+          SET @author = (
+            SELECT TOP 1 UPPER(A.last_name)
+            FROM T_Authors AS A
+              JOIN T_AuthorsBooks AS AB ON A.p_author_id = AB.p_f_author_id
+            WHERE AB.p_f_book_id = @book
+          );
 
-			SET @timestamp = DATEDIFF(SECOND, '1970-01-01', CURRENT_TIMESTAMP);
+          SET @timestamp = DATEDIFF(SECOND, '1970-01-01', CURRENT_TIMESTAMP);
 
-			SET @signature = CONCAT(@field, @author, @book, @count, @timestamp, CURRENT_REQUEST_ID());
+          SET @signature = CONCAT(@field, @author, @book, @count, @timestamp, CURRENT_REQUEST_ID());
 
-			INSERT INTO T_Exemplars (p_signature, p_f_book_id, exemplar_status)
-			VALUES (@signature, @book, @status);
-			
-			SET @count = @count - 1;
-			EXECUTE create_exemplars @book, @count, @status;
-		END
-	END
+          INSERT INTO T_Exemplars (p_signature, p_f_book_id, exemplar_status)
+          VALUES (@signature, @book, @status);
+      
+          SET @count = @count - 1;
+          EXECUTE create_exemplars @book, @count, @status;
+        END
+    END
 GO
